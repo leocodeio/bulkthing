f4d148b614db943f3eca811e167bf096e6ad5a15
leocodeio <basicleo787@gmail.com>
2025-10-21 15:05:39 +0530

    Basic api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
+"use client";
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
-      <Button>Click me</Button>
+      <Button
+        onClick={() => {
+          fetch("/api")
+            .then((res) => res.json())
+            .then((data) => console.log(data));
+        }}
+      >
+        Click me
+      </Button>
     </div>
   );
 }
